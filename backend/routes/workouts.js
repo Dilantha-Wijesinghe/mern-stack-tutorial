d912746144d9ac6be9684d32670db22bfd611143
Dilantha-Wijesinghe <wdilantha66@gmail.com>
2023-02-10 15:55:02 +0530

    mongodb

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,4 +1,4 @@
-const express=  require('express')
+const express = require('express')
 
 const router = express.Router()
 
@@ -32,4 +32,4 @@ router.patch('/:id', (req, res) => {
 
 
 
-module.export = router
+module.exports = router
